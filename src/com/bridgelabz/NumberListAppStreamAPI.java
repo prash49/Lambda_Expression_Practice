f7d8b73851d38ea79973784e85837474636369aa
Prashanth <imprash4u@gmail.com>
2021-09-22 19:08:38 +0530

    Added: Converting Integer to Double Usingn Function and Storing the result

--- src/com/bridgelabz/NumberListAppStreamAPI.java
+++ src/com/bridgelabz/NumberListAppStreamAPI.java
@@ -1,6 +1,8 @@
 package com.bridgelabz;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 
 public class NumberListAppStreamAPI {
     public static void main(String[] args) {
@@ -8,7 +10,13 @@ public class NumberListAppStreamAPI {
         list.add(1);
         list.add(4);
         list.add(6);
-        // creating stream  and Iterate the Each item to show each element of stream
+        // UC2.1 creating stream  and Iterate the Each item to show each element of stream
         list.stream().forEach(item -> System.out.println(item));
+
+        // UC2.2 converting and Storing
+        List<Double> doubleList = new ArrayList<>(); // for storing
+        Function<Integer, Double> doubleFunction = (n) -> n.doubleValue();
+        list.stream().forEach(item -> doubleList.add(doubleFunction.apply(item)));// converting & Storing
+        System.out.println(doubleList);
     }
 }
